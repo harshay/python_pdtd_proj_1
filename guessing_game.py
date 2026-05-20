2b95cb6da80103fdf747b501d7e168c10b1d603b
harshay <harshaykr@hotmail.com>
2026-05-20 08:53:25 +0100

    ready_for_submission

--- guessing_game.py
+++ guessing_game.py
@@ -19,25 +19,41 @@ def start_game():
     
 #   2. Store a random number as the answer/solution.
     answer = random.randint(1,10)
-    print(answer)  
-    
-    guess_count = 0
+    last_guess_count = 999999999
+    guess_count = 0    
+
+    while True:
 
 #   3. Continuously prompt the player for a guess.
-    while True:             
+        while True:             
+            
+            try:            
+                player_guess = int(input("Enter your guess "))
+                if player_guess < 1 or player_guess > 10:
+                    print("oops! you guess needs to be a number between 1 and 10. Try again.")
+            except ValueError: 
+                print("oh no! we ran into an issue. Please try again. Enter numbers only")
+            else: 
+                print("your guess is",player_guess) 
+                guess_count = guess_count + 1
+                print("Your total guesses are ",guess_count," ")
 
-        player_guess = int(input("Enter your guess "))
-        print("your guess is",player_guess) 
-        guess_count = guess_count + 1
-        print("Your total guesses are ",guess_count)
+                if player_guess > answer:
+                    print("It's Lower")   
+                elif player_guess < answer:
+                    print("It's Higher")
+                else:
+                    print("You got the answer in ",guess_count," guesses!")
+                    break
 
-        if player_guess > answer:
-            print("It's Lower")   
-        elif player_guess < answer:
-            print("It's Higher")
+        play_again_val = input("do you want to play again? Enter Yes if you want to play again, No if you don't ") 
+
+        if play_again_val.lower() == 'no':
+            print("The game will now end. Goodbye!")
+            break 
         else:
-            print("You got the answer in ",guess_count," guesses! The game will now end.")
-            break
+            if guess_count < last_guess_count:
+                last_guess_count = guess_count
+            print("your best score in this game is ",last_guess_count)
 
-                                 
 start_game()
